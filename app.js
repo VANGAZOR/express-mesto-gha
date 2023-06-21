bd383b5074023fb0c962246fe7cc24c98c62674a
VANGAZOR <asei3k@gmail.com>
2023-06-21 09:13:58 +0300

    test

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const regexUrl =
   /^(http[s]:\/\/)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]+(\.[a-zA-Z]{2,}([a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=])*)/;
-const { celebrate, Joi } = require("celebrate");
+const { celebrate, Joi, errors } = require("celebrate");
 const { HTTP_STATUS_NOT_FOUND } = require("http2").constants;
 const { login, createUser } = require("./controllers/users");
 // const { validationLogin, validationCreateUser } = require("./middlewares/auth");
@@ -14,7 +14,6 @@ const auth = require("./middlewares/auth");
 
 // подключаемся к серверу mongo
 mongoose.connect("mongodb://localhost:27017/mestodb", {});
-app.use(auth);
 app.post(
   "/signin",
   celebrate({
@@ -40,8 +39,9 @@ app.post(
   createUser
 );
 
+app.use(auth);
 app.use("/", require("./routes/index"));
-
+app.use(errors());
 app.use((req, res) => {
   res.status(HTTP_STATUS_NOT_FOUND).send({ message: "Неправильный путь" });
 });
